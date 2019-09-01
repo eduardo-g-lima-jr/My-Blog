75f2f7810a9b738d132e067a24576999bb57f80b
eduardo-g-lima-jr <49029478+eduardo-g-lima-jr@users.noreply.github.com>
2019-09-01 09:29:46 -0300

    salvando novas modificações

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,23 +1,24 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
 const AboutPage = () => (
   <>  
     <Layout>
-        <SEO title="About"/>
+      <SEO title="About"/>
+    
+      <h1> About Page </h1>
+      <ul>
+          <li>
+              <Link to="/about" activeStyle={{color: 'red'}}>About</Link>
+          </li>
+          <li>
+              <Link to="/" >Home</Link>
+          </li>
+      </ul>
     </Layout>
-    <h1> About Page </h1>
-    <ul>
-        <li>
-            <Link to="/about" activeStyle={{color: 'red'}}>About</Link>
-        </li>
-        <li>
-            <Link to="/" >Home</Link>
-        </li>
-    </ul>
   </>  
 )
 
