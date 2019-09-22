f4f1ab4f51fefa962e15878e64d11dc2dbe41742
eduardo-g-lima-jr <49029478+eduardo-g-lima-jr@users.noreply.github.com>
2019-09-22 14:08:26 -0300

    Correção do bug na index do PostItem

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -1,14 +1,23 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import * as S from './styled'
+import React from "react"
+import PropTypes from "prop-types"
 
-const PostItem = ({ slug, background, category, date, timeToRead, title, description }) => (
+import * as S from "./styled"
+
+const PostItem = ({
+  slug,
+  background,
+  category,
+  date,
+  timeToRead,
+  title,
+  description,
+}) => (
   <S.PostItemLink to={slug}>
     <S.PostItemWrapper>
       <S.PostItemTag background={background}>{category}</S.PostItemTag>
       <S.PostItemInfo>
         <S.PostItemDate>
-          {date} - {timeToRead}
+          {date} • {timeToRead} min de leitura
         </S.PostItemDate>
         <S.PostItemTitle>{title}</S.PostItemTitle>
         <S.PostItemDescription>{description}</S.PostItemDescription>
@@ -17,14 +26,14 @@ const PostItem = ({ slug, background, category, date, timeToRead, title, descrip
   </S.PostItemLink>
 )
 
-PostItem.PropTypes = {
+PostItem.propTypes = {
   slug: PropTypes.string.isRequired,
-  backgroud: PropTypes.string,
+  background: PropTypes.string,
   category: PropTypes.string.isRequired,
   date: PropTypes.string.isRequired,
   timeToRead: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired
+  description: PropTypes.string.isRequired,
 }
 
 export default PostItem
